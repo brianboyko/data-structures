a91b1f49c56eb4fa459eca80601b6f7c869866dd
MKS-students <mks.students@hackreactor.com>
2015-07-29 16:09:36 -0500

    Safety commit

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    size && size--;
+    var output = storage[0];
+    for(var key in storage){
+      storage[key] = storage[key+1];
+    }
+    return output;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
