4878a62c63facc175cfe5e6282a74d1c1ebb4356
MKS-students <mks.students@hackreactor.com>
2015-07-30 18:18:04 -0500

    implimented object prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Prototypal instantiation: using Object.create
 Do:
@@ -9,12 +9,29 @@ Don't:
   Example: https://github.com/makersquare/giraffeMaker/blob/master/src/giraffePrototype.js
 
 */
-
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var stack = Object.create(stackMethods);
+  stack.mySize = 0;
+
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this[this['mySize']] = value;  // this['foo'] is the same as this.foo
+    this.mySize++;
+  },
 
+  pop: function() {
+    if(this.mySize >0) {this.mySize--;}
+    var output = this[this['mySize']];
+    return output;
+  },
+  size: function() {
+    return this.mySize;
+  }
 
+};
